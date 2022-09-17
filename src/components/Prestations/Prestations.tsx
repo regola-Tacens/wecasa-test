616bbbb2026ec8408686a9f2feb770e58baaf0fc
regola-Tacens <regolas-tacens@outlook.fr>
2022-09-17 05:50:17 +0100

    added price to prestationList

--- src/components/Prestations/Prestations.tsx
+++ src/components/Prestations/Prestations.tsx
@@ -15,7 +15,7 @@ import PrestationsGenderSelector from './PrestationsGenderSelector';
 import {fetchPrestations} from '../../crud/prestations/prestations.crud';
 
 // types imports
-import {categoryType, GenderType} from 'src/types/haircutsType';
+import {categoryType, GenderType, prestationType} from 'src/types/haircutsType';
 
 const Prestations = () => {
   const [gender, setGender] = useState<GenderType>({reference: 'Man'})
@@ -27,12 +27,33 @@ const Prestations = () => {
   const prestationsByGender = useMemo(():categoryType[] => {
     return haircutsData?.categories?.filter((category: categoryType) =>
       category.reference === gender.reference.toLowerCase())[0].prestations
-  }, [gender, haircutsData?.categories])
+  }, [gender, haircutsData?.categories]);
+
+  const prestationsListTemplate = (option: prestationType) => {
+    return (
+      <div className='country-item'>
+        {option.price}
+        <div>{option.title}</div>
+      </div>
+    );
+  };
 
   return (
     <div>
       <PrestationsGenderSelector setGender={setGender}/>
-      <ListBox value={prestationsByGender} options={prestationsByGender} optionLabel='title' style={{width: '15rem', maxHeight: '20rem', overflow: 'auto'}} onChange={e => console.log(e.value)}/>
+      <ListBox
+        value={prestationsByGender}
+        options={prestationsByGender}
+        optionLabel='title'
+        style={
+          {
+            width: '15rem',
+            maxHeight: '250px',
+            overflow: 'auto'}
+        }
+        onChange={e => console.log(e.value)}
+        itemTemplate={prestationsListTemplate}
+      />
       <Spinner loading={haircutStatus} />
       <ErrorToast error={haircutsError} />
     </div>
